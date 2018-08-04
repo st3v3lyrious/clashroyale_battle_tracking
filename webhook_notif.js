ab5e5f30e1b3985def513d0515b416d6f9f0b00d
Steve Alomassor <stevylirious@gmail.com>
2018-08-04 11:44:47 +0200

    change request to request-promise and use async to return the war summary first

--- webhook_notif.js
+++ webhook_notif.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const request = require("request-promise");
 const config = require("./conf.json");
 
 let options = { method: 'GET',
@@ -15,30 +15,32 @@ let webhook_options = {
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
-  if(response.statusCode === 200){
+  if(response.statusCode === 200) {
     let war_data = JSON.parse(body);
-    if(war_data.state == 'warDay'){
+    if(war_data.state == 'warDay') {
       let warday_summary = `It's war day! There are ${war_data.clan.participants} participants in this war.`;
       request
-      .post(webhook_options.webhook_url)
-      .form({
-        username: `${config.webhook_username}`,
-        content: '```css' +'\n' + warday_summary + '\n' +'```',
-        avatar_url: webhook_options.avatar_url
-      })
-      if(war_data.clan.battlesPlayed !== war_data.clan.participants){
-        for(i = 0; i<war_data.participants.length; i++){
-          if(war_data.participants[i].battlesPlayed === 0){
-            request
-              .post(webhook_options.webhook_url)
-              .form({
-                username: `${config.webhook_username}`,
-                content: '```css' +'\n' +`${war_data.participants[i].name} hasn't played all his battles` + '\n' +'```',
-                avatar_url: webhook_options.avatar_url
-              })
+        .post(webhook_options.webhook_url)
+        .form({
+          username: `${config.webhook_username}`,
+          content: '```css' +'\n' + warday_summary + '\n' +'```',
+          avatar_url: webhook_options.avatar_url
+        })
+        .then(function() {
+          if(war_data.clan.battlesPlayed !== war_data.clan.participants) {
+            for(i = 0; i<war_data.participants.length; i++){
+              if(war_data.participants[i].battlesPlayed === 0){
+                request
+                  .post(webhook_options.webhook_url)
+                  .form({
+                    username: `${config.webhook_username}`,
+                    content: '```css' +'\n' +`${war_data.participants[i].name} hasn't played all his battles` + '\n' +'```',
+                    avatar_url: webhook_options.avatar_url
+                  })
+              }
+            }
           }
-        }
-      }
+        })
     } else {
       let collectionday_summary = `It's collection day! There are currently ${war_data.clan.participants} participants in this war.`;
       request
@@ -48,17 +50,21 @@ request(options, function (error, response, body) {
           content: '```css' +'\n' + collectionday_summary + '\n' +'```',
           avatar_url: webhook_options.avatar_url
         })
-      for(i = 0; i<war_data.participants.length; i++){
-        if(war_data.participants[i].battlesPlayed !== 3){
-          request
-          .post(webhook_options.webhook_url)
-          .form({
-            username: `${config.webhook_username}`,
-            content: '```css' +'\n' + `${war_data.participants[i].name} hasn't played all his battles` + '\n' +'```',
-            avatar_url: webhook_options.avatar_url
-          })
-        }
-      }
+        .then(function() {
+          for(i = 0; i<war_data.participants.length; i++){
+            if(war_data.participants[i].battlesPlayed !== 3){
+              request
+              .post(webhook_options.webhook_url)
+              .form({
+                username: `${config.webhook_username}`,
+                content: '```css' +'\n' + `${war_data.participants[i].name} hasn't played all his battles` + '\n' +'```',
+                avatar_url: webhook_options.avatar_url
+              })
+            }
+          }
+        }).catch(function(error) {
+          console.log("An error happened", error);
+        })
     }
   }else {
     console.log('The request failed');
